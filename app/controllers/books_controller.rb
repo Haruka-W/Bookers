5ea456d5b551c1fbe000e80e8f011b0a6b48b9d9
Haruka-W <nekonoko1995@gmail.com>
2020-02-12 15:12:54 +0900

    [Fix]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,7 +19,8 @@ class BooksController < ApplicationController
   def create
   	book = Book.new(book_params)
   	book.save
-  	redirect_to books_path
+    flash[:notice] = "Book was successfully created."
+  	redirect_to book_path(book)
   end
 
   def edit
@@ -29,13 +30,15 @@ class BooksController < ApplicationController
   def update
   	book = Book.find(params[:id])
   	book.update(book_params)
-  	redirect_to book_path(book)
+    book.save
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(book)
   end
 
   def destroy
   	book = Book.find(params[:id])
   	book.destroy
-  	redirect_to book_path
+  	redirect_to books_path
   end
 
   private
